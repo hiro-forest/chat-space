ccadf5ca4af560d36fd5bbbdebcd21b1c6f98fe2
hiro-forest <h.ginnoringokara41@icloud.com>
2019-11-18 18:17:45 +0900

    インクリメンタルサーチ

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,76 @@
+$(function() {
+  function addUser(user){
+    let html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+    </div>
+    `;
+    $("#user-search-result").append(html);
+  }
+
+  function addNoUser(){
+    let html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">ユーザーが見つかりません</p>
+    </div>
+    `;
+    $("#user-search-result").append(html); 
+  }
+
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class="ChatMember__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".ChatMembers").append(html);
+  }
+
+  function addMember(userId) {
+    let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
+  }
+
+  $("#user-search-field").on("keyup", function() {
+    let input = $("#user-search-field").val();
+    $.ajax({
+      url: "/users",
+      type: "GET",
+      data: { keyword: input},  
+      dataType: 'json'
+    })
+      .done(function(users){
+        $("#user-search-result").empty();
+        
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            addUser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+
+      .fail(function(){
+        alert("ユーザー検索に失敗しました");
+      });
+  });
+  $(document).on("click", ".chat-group-user__btn--add", function() {
+    console.log
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+    addMember(userId);
+  });
+  $(document).on("click", ".chat-group-user__btn--remove", function() {
+    $(this)
+      .parent()
+      .remove();
+  });
+});
