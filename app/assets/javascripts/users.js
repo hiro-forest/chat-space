9398e3fd45c65e74fcaad78c4793e22573ce9d10
hiro-forest <h.ginnoringokara41@icloud.com>
2019-11-19 17:39:45 +0900

    自動更新機能の実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,48 +1,45 @@
 $(function() {
-  function addUser(user){
+  function addUser(user) {
     let html = `
-    <div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">${user.name}</p>
-    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
-    </div>
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
     `;
     $("#user-search-result").append(html);
   }
 
-  function addNoUser(){
+  function addNoUser() {
     let html = `
-    <div class="chat-group-user clearfix">
-      <p class="chat-group-user__name">ユーザーが見つかりません</p>
-    </div>
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
     `;
-    $("#user-search-result").append(html); 
+    $("#user-search-result").append(html);
   }
-
   function addDeleteUser(name, id) {
     let html = `
-    <div class="ChatMember clearfix" id="${id}">
-      <p class="ChatMember__name">${name}</p>
-      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
     </div>`;
-    $(".ChatMembers").append(html);
+    $(".js-add-user").append(html);
   }
-
   function addMember(userId) {
     let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
     $(`#${userId}`).append(html);
   }
-
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
-      url: "/users",
       type: "GET",
-      data: { keyword: input},  
-      dataType: 'json'
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
     })
-      .done(function(users){
+      .done(function(users) {
         $("#user-search-result").empty();
-        
+
         if (users.length !== 0) {
           users.forEach(function(user) {
             addUser(user);
@@ -53,9 +50,8 @@ $(function() {
           addNoUser();
         }
       })
-
-      .fail(function(){
-        alert("ユーザー検索に失敗しました");
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示できません。");
       });
   });
   $(document).on("click", ".chat-group-user__btn--add", function() {
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.user_name  @message.user.name
 json.date  @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.content  @message.content
 json.image @message.image.url
+json.id @message.id
